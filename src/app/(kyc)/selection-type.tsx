cc9e727187d55479b646ea4e8edbddb46fa91712
gr1ntch <odewumimighty@gmail.com>
2025-09-08 21:04:10 +0100

    feat(KYC): Add initial KYC screens and components for user verification flow

---
+++ src/app/(kyc)/selection-type.tsx
@@ -0,0 +1,11 @@
+import { View, Text, } from "react-native";
+ 
+export default function SelectionType() {
+  return (
+    <>
+      <View>
+        <Text>Selection Type</Text>
+      </View>
+    </>
+  )
+}
