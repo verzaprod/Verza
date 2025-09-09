ff1b2171856340b10ea9fd48cd72a384a7c25dab
gr1ntch <odewumimighty@gmail.com>
2025-09-09 21:21:04 +0100

    fix(Success): Remove unused imports and clean up code structure

--- src/components/ui/Success.tsx
+++ src/components/ui/Success.tsx
@@ -4,8 +4,17 @@ import { useTheme } from "@/theme/ThemeProvider";
 import { useRouter } from "expo-router";
 import { View, Text, KeyboardAvoidingView, Platform } from "react-native";
 
-export default function Success({ redirectType, tagline, title, buttonText }: { redirectType: string, tagline: string, title: string, buttonText: string }) {
-  
+export default function Success({
+  redirectType,
+  tagline,
+  title,
+  buttonText,
+}: {
+  redirectType: string;
+  tagline: string;
+  title: string;
+  buttonText: string;
+}) {
   const theme = useTheme();
   const router = useRouter();
 
@@ -15,16 +24,20 @@ export default function Success({ redirectType, tagline, title, buttonText }: { 
     } else {
       router.replace("/(kyc)/selection-type");
     }
-  }
+  };
 
   return (
-    <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === "ios" ? "padding" : "height"}>
-      <View className="flex-1 items-center justify-center px-6"
-        style={{ 
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View
+        className="flex-1 items-center justify-center px-6"
+        style={{
           backgroundColor: theme.colors.background,
         }}
       >
-        <Icon name="success" size={100}/>
+        <Icon name="success" size={100} />
 
         <Text
           className="text-3xl"
@@ -46,11 +59,8 @@ export default function Success({ redirectType, tagline, title, buttonText }: { 
           {tagline}
         </Text>
 
-        <CTAButton 
-          title={buttonText}
-          onPress={handleClick}
-        />
+        <CTAButton title={buttonText} onPress={handleClick} />
       </View>
     </KeyboardAvoidingView>
-  )
+  );
 }
